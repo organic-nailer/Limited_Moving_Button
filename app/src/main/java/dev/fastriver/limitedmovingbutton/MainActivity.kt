3bcc83abe28346b47acb2fc20a965ba32580d1ef
fastriver <hykwyuk0125@gmail.com>
2020-02-02 17:57:29 +0900

    戻るDraggableButton

--- app/src/main/java/dev/fastriver/limitedmovingbutton/MainActivity.kt
+++ app/src/main/java/dev/fastriver/limitedmovingbutton/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.PI
 import kotlin.math.atan
 
 class MainActivity : AppCompatActivity() {
 
-    var touchGapY: Float = 0f
-    var startRawY: Float = 0f
+    private var touchGapY: Float = 0f
+    private var startRawY: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +33,27 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 MotionEvent.ACTION_UP -> {
-                    println("終了座標: ${limitedButton.y}")
+                    goBack2MyRoots(startRawY - touchGapY)
                 }
             }
 
             true
         }
     }
+
+    fun goBack2MyRoots(root: Float) {
+        val originAnimation = limitedButton?.let {
+            SpringAnimation(it, DynamicAnimation.Y, root)
+        }
+
+        originAnimation?.run {
+            spring.stiffness = SpringForce.STIFFNESS_MEDIUM
+            spring.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        }
+
+        originAnimation?.start()
+    }
+
 }
 
 fun limiter(input: Float, limit: Float, variation: Float): Float {
